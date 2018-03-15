f774100747b11cba5e9dc282d864e7dabdb1b29a
teje87 <mikeltejero1987@gmail.com>
2018-03-15 15:44:20 +0100

    fs-menu issue fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
     $(function() {                       //run when the DOM is ready
         $(".hamburger--collapse").click(function() {  //use a class, since your ID gets mangled
           $(this).toggleClass("is-active");      //add the class to the clicked element
+          $(".fs-menu").toggleClass("fs-menu-active");
         });
       });
 
